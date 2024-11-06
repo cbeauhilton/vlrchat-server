0ee46a5d60e4685be967ce9c520fd28526437dbe
Beau <beau@beauhilton.com>
2024-11-06 16:35:59 -0600

    clean

--- authentik.nix
+++ authentik.nix
@@ -12,6 +12,8 @@
     "d /var/lib/authentik/media 0750 authentik authentik -"
     "d /var/lib/authentik/certs 0750 authentik authentik -"
     "d /var/lib/authentik/templates 0750 authentik authentik -"
+    "d /var/lib/authentik 0750 authentik authentik -"
+    "f /var/lib/authentik/authentik.env 0640 authentik authentik - AUTHENTIK_SECRET_KEY=yoursecretkeyhere\nAUTHENTIK_POSTGRESQL__HOST=localhost\nAUTHENTIK_POSTGRESQL__USER=authentik\nAUTHENTIK_POSTGRESQL__NAME=authentik\nAUTHENTIK_POSTGRESQL__PASSWORD=authentik"
   ];
 
   # Enable Authentik service
@@ -36,12 +38,6 @@
     # Add environment configuration
     environmentFile = "/var/lib/authentik/authentik.env";
   };
-
-  # Create basic environment file
-  systemd.tmpfiles.rules = [
-    "d /var/lib/authentik 0750 authentik authentik -"
-    "f /var/lib/authentik/authentik.env 0640 authentik authentik - AUTHENTIK_SECRET_KEY=yoursecretkeyhere\nAUTHENTIK_POSTGRESQL__HOST=localhost\nAUTHENTIK_POSTGRESQL__USER=authentik\nAUTHENTIK_POSTGRESQL__NAME=authentik\nAUTHENTIK_POSTGRESQL__PASSWORD=authentik"
-  ];
 
   # Enable and configure PostgreSQL
   services.postgresql = {
