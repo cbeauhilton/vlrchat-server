6fb0d4514aea9716c6d4e8cb4afb5bc919c49e7c
Beau <beau@beauhilton.com>
2024-11-06 13:52:27 -0600

    authentik, nginx

--- authentik.nix
+++ authentik.nix
@@ -7,9 +7,13 @@
       name = "authentik";
       ensureDBOwnership = true;
     }];
+    settings = {
+      listen_addresses = "localhost";
+    };
     authentication = pkgs.lib.mkOverride 10 ''
       # TYPE  DATABASE        USER            ADDRESS         METHOD
       local   authentik       authentik                       trust
+      host    authentik       authentik       127.0.0.1/32   trust
       local   all            all                             peer
     '';
   };
@@ -25,7 +29,6 @@
     };
   };
 
-  # Add tmpfiles rules for Redis socket directory
   systemd.tmpfiles.rules = [
     "d /var/lib/authentik/data 0750 authentik authentik -"
     "d /var/lib/authentik/media 0750 authentik authentik -"
@@ -37,22 +40,16 @@
     environmentFiles = [ "/run/secrets/authentik/authentik-env" ];
     
     environment = {
-      AUTHENTIK_POSTGRESQL__HOST = "/var/run/postgresql";
+      AUTHENTIK_POSTGRESQL__HOST = "127.0.0.1";
       AUTHENTIK_POSTGRESQL__USER = "authentik";
       AUTHENTIK_POSTGRESQL__NAME = "authentik";
       AUTHENTIK_REDIS__HOST = "127.0.0.1";
       AUTHENTIK_REDIS__PORT = "6379";
     };
     
-    ports = [
-      "9000:9000"  # HTTP
-      "9443:9443"  # HTTPS
-    ];
-
     volumes = [
       "/var/lib/authentik/data:/data"
       "/var/lib/authentik/media:/media"
-      "/var/run/postgresql:/var/run/postgresql" # Mount PostgreSQL socket directory
     ];
 
     dependsOn = [ "postgresql.service" "redis-authentik.service" ];
--- linux.nix
+++ linux.nix
@@ -23,6 +23,7 @@
   programs.zsh.enable = true;
   environment.pathsToLink = ["/share/zsh"];
   environment.shells = [pkgs.zsh];
+  services.nginx.enable = true;
 
   environment.enableAllTerminfo = true;
 
