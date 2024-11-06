5208bc5d3308bf7ba9aa342f8286c007ab782ba2
Beau <beau@beauhilton.com>
2024-11-06 16:24:33 -0600

    even simpler

--- nginx-authentik.nix
+++ nginx-authentik.nix
@@ -1,10 +1,4 @@
 { config, pkgs, ... }: {
-  # Create the static directory and HTML file
-  systemd.tmpfiles.rules = [
-    "d /var/lib/authentik/static 0755 authentik authentik -"
-    "f /var/lib/authentik/static/index.html 0644 authentik authentik - <!DOCTYPE html><html><head><title>Auth Test Page</title></head><body><h1>Auth Test Page</h1><p>If you can see this, nginx is working correctly with SSL!</p></body></html>"
-  ];
-
   services.nginx = {
     enable = true;
     
@@ -17,9 +11,7 @@
     # Simple virtual host configuration
     virtualHosts."auth.vlr.chat" = {
       enableACME = false;
-      forceSSL = true;
-      sslCertificate = "/var/lib/authentik/certs/cert.pem";
-      sslCertificateKey = "/var/lib/authentik/certs/key.pem";
+      forceSSL = false;
       
       # Serve a static HTML page
       root = "/var/lib/authentik/static";
@@ -28,4 +20,10 @@
       };
     };
   };
+
+  # Create the static directory and HTML file
+  systemd.tmpfiles.rules = [
+    "d /var/lib/authentik/static 0755 authentik authentik -"
+    "f /var/lib/authentik/static/index.html 0644 authentik authentik - <!DOCTYPE html><html><head><title>Auth Test Page</title></head><body><h1>Auth Test Page</h1><p>If you can see this, nginx is working correctly!</p></body></html>"
+  ];
 }
