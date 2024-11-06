1c2e263f5677b6e633ee04bfd34713de013905db
beau hilton <cbeauhilton@gmail.com>
2024-11-06 08:19:46 -0600

    initial

---
+++ linux.nix
@@ -0,0 +1,87 @@
+{
+  # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
+  # secrets,
+  username,
+  hostname,
+  pkgs,
+  inputs,
+  ...
+}: {
+  time.timeZone = "America/Chicago";
+
+  systemd.tmpfiles.rules = [
+    "d /home/${username}/.config 0755 ${username} users"
+    "d /home/${username}/.config/lvim 0755 ${username} users"
+  ];
+
+  networking.hostName = "${hostname}";
+
+  programs.zsh.enable = true;
+  environment.pathsToLink = ["/share/zsh"];
+  environment.shells = [pkgs.zsh];
+
+  environment.enableAllTerminfo = true;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users.${username} = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
+    openssh.authorizedKeys.keys = [
+      (builtins.readFile ./id_ed25519_hetzner_.pub)
+    ];
+  };
+
+  home-manager.users.${username} = {
+    imports = [
+      ./home.nix
+    ];
+  };
+
+  system.stateVersion = "22.05";
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    autoPrune.enable = true;
+  };
+
+  nix = {
+    settings = {
+      trusted-users = [username];
+      # FIXME: use your access tokens from secrets.json here to be able to clone private repos on GitHub and GitLab
+      # access-tokens = [
+      #   "github.com=${secrets.github_token}"
+      #   "gitlab.com=OAuth2:${secrets.gitlab_token}"
+      # ];
+
+      accept-flake-config = true;
+      auto-optimise-store = true;
+    };
+
+    registry = {
+      nixpkgs = {
+        flake = inputs.nixpkgs;
+      };
+    };
+
+    nixPath = [
+      "nixpkgs=${inputs.nixpkgs.outPath}"
+      "nixos-config=/etc/nixos/configuration.nix"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
+
+    package = pkgs.nixFlakes;
+    extraOptions = ''experimental-features = nix-command flakes'';
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 7d";
+    };
+  };
+}
+
