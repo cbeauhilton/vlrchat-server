ef223e49e4a5dc647dfd48ce96bf47998d124dd1
Beau <beau@beauhilton.com>
2024-11-07 15:00:31 -0600

    traefik, flowise

--- modules/backend-services/default.nix
+++ modules/backend-services/default.nix
@@ -3,7 +3,7 @@ with lib; let
   cfg = config.services.vlr.backend;
 in {
   imports = [
-    ./flowise
+    # ./flowise
     ./static
   ];
 
@@ -13,16 +13,16 @@ in {
 
   config = mkIf cfg.enable {
     # Enable individual services if not explicitly disabled
-    services.vlr.backend.flowise.enable = mkDefault false;
+    # services.vlr.backend.flowise.enable = mkDefault false;
     services.vlr.backend.static.enable = mkDefault true;
 
     # Common Traefik configuration
     services.traefik.dynamicConfigOptions.http = {
       # Services
       services = {
-        flowise.loadBalancer.servers = [{
-          url = "http://localhost:3000";
-        }];
+        # flowise.loadBalancer.servers = [{
+        #   url = "http://localhost:3000";
+        # }];
         static.loadBalancer.servers = [{
           url = "http://localhost:3001";
         }];
@@ -30,13 +30,13 @@ in {
 
       # Routers
       routers = {
-        flowise = {
-          entryPoints = ["websecure"];
-          rule = "Host(`flowise.vlr.chat`)";
-          service = "flowise";
-          middlewares = ["authentik"];
-          tls.certResolver = "letsencrypt";
-        };
+        # flowise = {
+        #   entryPoints = ["websecure"];
+        #   rule = "Host(`flowise.vlr.chat`)";
+        #   service = "flowise";
+        #   middlewares = ["authentik"];
+        #   tls.certResolver = "letsencrypt";
+        # };
         static = {
           entryPoints = ["websecure"];
           rule = "Host(`static.vlr.chat`)";
